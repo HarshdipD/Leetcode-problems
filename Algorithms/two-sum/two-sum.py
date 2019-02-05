04913abc3527832a3d32ada55f8515b4ce4e2d64
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2019-02-05 16:42:22 -0500

    Create two-sum.py

---
+++ Algorithms/two-sum/two-sum.py
@@ -0,0 +1,18 @@
+"""
+Novice solutions:
+Time-complexity: o(n2)  40ms
+
+class Solution:
+    def twoSum(self, nums, target):
+        for a in nums:
+            for b in nums:
+                if(a+b == target):
+                    result = [nums.index(a), nums.index(b)]
+                    return result
+"""
+
+#Time complexity: 
+
+class Solution:
+    def twoSum(self, nums, target):
+        
