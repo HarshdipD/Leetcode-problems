d156416dff82533524187099eb8f960c35b423a2
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2019-03-20 17:59:29 -0400

    Update two-sum.py

--- Python3/two-sum.py
+++ Python3/two-sum.py
@@ -1,18 +1,13 @@
-'''
-Novice solutions:
-Time-complexity: o(n2)  40ms
-
 class Solution:
-    def twoSum(self, nums, target):
-        for a in nums:
-            for b in nums:
-                if(a+b == target):
-                    result = [nums.index(a), nums.index(b)]
-                    return result
-'''
-
-#Time complexity: 
-
-class Solution:
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) < 2:
+            return []
         
+        hashtable = {}
+        for a in range(len(nums)):
+            value = nums[a]
+            comp = target - value
+            if (comp not in hashtable):
+                hashtable[value] = a
+            else:
+                 return [hashtable[comp], a]
