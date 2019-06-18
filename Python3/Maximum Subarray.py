8af6daee96e579582e3f27bc0cd475690c017763
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2019-06-18 11:52:03 -0400

    Update Maximum Subarray.py

--- Python3/Maximum Subarray.py
+++ Python3/Maximum Subarray.py
@@ -1,5 +1,6 @@
 #Explanation: https://leetcode.com/problems/maximum-subarray/discuss/185856/Explanation-of-the-O(n)-solution-in-Python
 
+'''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum = max(nums)
@@ -11,3 +12,13 @@ class Solution:
             if cur_sum < 0:
                 cur_sum = 0
         return max_sum
+ '''
+
+#Found a better, shorter solution. The explanation relains the same
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        for a in range(1, len(nums)):
+            if nums[a-1] > 0:
+                nums[a] = nums[a] + nums[a-1]
+        return max(nums)
